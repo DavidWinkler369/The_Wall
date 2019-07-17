62f9c541cb05d30310762e628a6590c49595a30f
JWeber2113 <weberjoseph9393@yahoo.com>
2019-07-17 09:02:18 -0700

    added register and log in routes and set up templates and added a little more functionality to views.py methods

--- apps/wall_app/views.py
+++ apps/wall_app/views.py
@@ -10,11 +10,34 @@ def index(request):
 def register(request):
     print('*'*100)
     print('Registering...')
+    errors = User.objects.registrationValidation(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.create(
+            firstName = request.POST['fname'],
+            lastName = request.POST['lname'],
+            email_address = request.POST['email'],
+            password = bcrypt.hashpw(request.POST['pword'].encode(), bcrypt.gensalt())
+        )
+        request.session['user'] = user.id
+        request.session['fname'] = user.first_name
     return redirect('/wall')
 
 def login(request):
     print('*'*100)
     print('logging in...')
+    errors = User.objects.loginValidation(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.filter(email_address = request.POST['email'])
+        request.session['user'] = user[0].id
+        request.session['fname'] = user[0].first_name
     return redirect('/wall')
 
 
